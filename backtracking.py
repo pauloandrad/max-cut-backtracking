8f9067557f7a06eed2f61065216605c244e6f4a1
paulo andrade <pauloh8755@outlook.com.br>
2026-04-21 17:27:33 -0300

    feat: add backtracking

---
+++ backtracking.py
@@ -0,0 +1,83 @@
+def is_partial_solution(top_score, edges, nodes, col, max_col):
+    score = calculate_scores(edges, nodes, col)
+    possible_points = max_col - col
+
+    if score + possible_points > top_score[0]:
+        return True
+
+    return False
+
+
+def populate_node_groups_recursive(solution, top_score, nodes, col, max_col, edges):
+    group = 0
+
+    if col == max_col:
+        score = calculate_scores(edges, nodes, col)
+
+        if score > top_score[0]:
+            top_score[0] = score
+            solution[0] = nodes.copy()
+
+        return
+
+    while group <= 1:
+        nodes.append(group)
+
+        if is_partial_solution(top_score, edges, nodes, col, max_col):
+            populate_node_groups_recursive(solution, top_score, nodes, col + 1, max_col, edges)
+
+        nodes.pop()
+        group += 1
+
+def initialize_node_groups(max_col, edges):
+    top_score = [0]
+    solution = [[]]
+    nodes = []
+    node_col = 0
+
+    populate_node_groups_recursive(solution, top_score, nodes, node_col, max_col, edges)
+
+    print(top_score)
+
+    return solution
+
+def initialize_edges(edges):
+    edge_list = []
+
+    for edge in range(edges):
+        if edge == edges - 1:
+            edge_list.append((edge, 0))
+            continue
+
+        edge_list.append((edge, edge + 1))
+
+    return edge_list
+
+def calculate_scores(edges, nodes, col):
+    score = 0
+
+    for i, edge in enumerate(edges):
+        if i >= col:
+            return score
+
+        if nodes[edge[0]] != nodes[edge[1]]:
+            score = score + 1
+
+    return score
+
+
+if __name__ == '__main__':
+    max_graph = 200
+
+    all_edges = initialize_edges(max_graph)
+    solution_nodes = initialize_node_groups(max_graph, all_edges)
+
+    print(all_edges)
+    print(solution_nodes)
+
+
+
+
+
+
+
--- brute_force.py
+++ brute_force.py
@@ -1,5 +1,5 @@
-def populate_node_groups_recursive(nodes_list, nodes, col):
-    if col == 4:
+def populate_node_groups_recursive(nodes_list, nodes, col, max_col):
+    if col == max_col:
         nodes_list.append(nodes.copy())
         return
 
@@ -7,21 +7,30 @@ def populate_node_groups_recursive(nodes_list, nodes, col):
 
     while group <= 1:
         nodes.append(group)
-        populate_node_groups_recursive(nodes_list, nodes, col + 1)
+        populate_node_groups_recursive(nodes_list, nodes, col + 1, max_col)
         nodes.pop()
         group += 1
 
-def initialize_node_groups():
+def initialize_node_groups(max_col):
     node_list = []
     nodes = []
     node_col = 0
 
-    populate_node_groups_recursive(node_list, nodes, node_col)
+    populate_node_groups_recursive(node_list, nodes, node_col, max_col)
 
     return node_list
 
-def initialize_edges():
-    return [(0,1), (1,2), (2,3), (3,0)]
+def initialize_edges(edges):
+    edge_list = []
+
+    for edge in range(edges):
+        if edge == edges - 1:
+            edge_list.append((edge, 0))
+            continue
+
+        edge_list.append((edge, edge + 1))
+
+    return edge_list
 
 def calculate_scores(edges, node_list):
     all_score = []
@@ -33,14 +42,17 @@ def calculate_scores(edges, node_list):
             if nodes[edge[0]] != nodes[edge[1]]:
                 score = score + 1
 
+        print(score)
         all_score.append(score)
 
     return all_score
 
 
 if __name__ == '__main__':
-    groups_list = initialize_node_groups()
-    all_edges = initialize_edges()
+    max_graph = 4
+
+    groups_list = initialize_node_groups(max_graph)
+    all_edges = initialize_edges(max_graph)
     all_scores = calculate_scores(all_edges, groups_list)
 
     max_points = [i for i, x in enumerate(all_scores) if x == max(all_scores)]
